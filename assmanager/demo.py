36ca60afef369129a8bda57a02d577315f16a088
zyzhao0926 <zyzh@smail.nju.edu.cn>
2023-10-12 15:28:42 +0800

    update README.md

--- assmanager/demo.py
+++ assmanager/demo.py
@@ -1,12 +1,14 @@
 from assmanager import AssManager
 
-inflation_values = [1.05]
+inflation_values = [1.01]
 inflation_sequences = ['before_DA']
 ensemble_size = 2000
-forcings = [16, 15]
+forcings = [15]
 time_steps = 200 * 360 * 5
 # time_steps = 200 * 20
 configs = []
+localization_method = None if ensemble_size >= 2000 else 'GC'
+file_save_option = 'multiple_files' if ensemble_size >= 2000 else 'single_file'
 
 for inf in inflation_values:
     for seq in inflation_sequences:
@@ -26,6 +28,7 @@ for inf in inflation_values:
                         'ensemble_size': ensemble_size,
                         'inflation_factor': inf,
                         'inflation_sequence': seq,
+                        'localization_method': localization_method,
                         'filter': 'EnKF',
                     },
                     
@@ -33,12 +36,12 @@ for inf in inflation_values:
                         'save_kalman_gain': True,
                         'save_prior_ensemble': True,
                         'save_analysis_ensemble': True,
-                        'file_save_option': 'multiple_files',
+                        'file_save_option': file_save_option,
                         # 'file_save_option': 'single_file',
                     },
 
                     'Experiment_option': {
-                        'experiment_name': f'EnKF_F{forcing}_inf_{inf}_{seq}_sz{ensemble_size}_5y_cpptest',
+                        'experiment_name': f'EnKF_F{forcing}_inf_{inf}_loc_{localization_method}_{seq}_sz{ensemble_size}_5y',
                         'result_save_path': '/mnt/pve_nfs/zyzhao/L05_experiments',
                     }
                 }
